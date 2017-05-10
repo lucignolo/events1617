98f489834b6abc6783fdae1210eb504e57027156
anna cristini <acristini028@gmail.com>
2017-05-10 18:36:17 +0200

    fine prima parte visitare-rails4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # definiamo store#index come la root del sito web
+  root 'store#index', as: 'store'
+  #
 end
